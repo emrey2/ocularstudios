f6f0974920ccc8c1b625b046256462ac7312d682
emrey2 <emrewp2@gmail.com>
2022-05-14 17:55:16 +0300

    +threediension p

--- src/components/ThreeDimension.js
+++ src/components/ThreeDimension.js
@@ -27,13 +27,17 @@ const ThreeDimension = () => {
       <div className="left">
         <h1>Web3</h1>
         <p>
-          With a passion for bringing the highest-quality visuals to latest
-          immersive experiences in the metaverse, VR studio is uniquely placed
-          to call on the creative studio’s team of talented artists and
-          technologists in order to create new virtual worlds. The teams’
-          projects include location-based entertainment, theme park rides and VR
-          experiences for some of the world’s biggest entertainment and consumer
-          brands. ​
+          What is Web 3.0? Delegating content control to software, that is, the
+          semantic web, in other words, the "artificial intelligence web", can
+          be called the new web world where we can discover what we are
+          interested in thanks to databases that talk to each other, and the
+          software will offer us and cover all devices connected to the
+          internet. This whole plot is connected to the concept of the semantic
+          web. What is the semantic web? In its simplest form, it means the
+          meaning of words. Web 3.0 shall be understood as meaning, not the word
+          integrity of the articles. Search engines will become smarter. It will
+          understand what we are looking for and want to understand and will lay
+          the foundations for new technology.
         </p>
         <Link to="/contact">
           <button className="btn">Contact</button>
